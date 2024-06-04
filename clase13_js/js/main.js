26b56863e75b7c13cc9bb7d178191f883d5cfd24
Federico Rabanedo <federico.rabanedo@gmail.com>
2024-06-03 21:38:58 -0300

    reinicio de repo

---
+++ clase13_js/js/main.js
@@ -0,0 +1,21 @@
+console.log("hola mundo");
+
+var nombre = "federico"
+
+
+var apellido ="Rabanedo"
+
+
+var edad ="26"
+
+
+var tenerMascotas=true
+
+
+console.log(nombre, apellido, edad, tenerMascotas);
+
+let entrada =prompt("ingrese su nombre");
+let salida ="Hola " + entrada;
+
+alert (salida)
+
