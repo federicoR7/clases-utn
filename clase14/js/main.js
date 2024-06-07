d58651ce4319a47132950a8e477d946e0cea8975
Federico Rabanedo <federico.rabanedo@gmail.com>
2024-06-07 01:51:48 -0300

    practicas js

--- clase14/js/main.js
+++ clase14/js/main.js
@@ -38,7 +38,7 @@ switch (dia) {
 }*/
 
 
-let numero = prompt("cuanto es 2+2?")
+/*let numero = prompt("cuanto es 2+2?")
 if (numero == 4) {
   alert("correcto");
 } else if (numero > 4) {
@@ -46,4 +46,33 @@ if (numero == 4) {
 } else if(numero < 4){
   document.write("incorrecto");
 }
+
+
+
+let numero = prompt("escribí un número mayor a 1000");
+if (numero > 1000){
+  alert("el número " + numero + " es mayor a 1000");
+} else if(numero => 1000){
+  alert("el número que elegise no es mayor que 1000");
+}
+
+let texto =prompt("escribí la palabra HOLA tal cual la se muestra");
+if(texto === "HOLA"){
+  document.write("ingresaste HOLA correctamente");
+} else if(texto !== "HOLA"){
+  document.write("mi loco, no lo escribiste igual");
+}*/
+
+
+let numero=prompt("dame un número entre 10 y 50");
+if (numero < 10){
+  alert("no es valido");
+} if(numero > 50){
+  alert("no es valido");
+} else if (numero => 10){
+  alert("correcto");
+} else if(numero <= 50){
+  alert("correcto");
+}
+
 
